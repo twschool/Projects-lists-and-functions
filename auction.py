3b6e50ddd9fc18289d01df089c1720ea2874054f
twschool <98859425+twschool@users.noreply.github.com>
2023-02-15 10:31:25 +1300

    Added my scuffed code
    Added the auction program. (All code fits on my screen)

---
+++ auction.py
@@ -0,0 +1,25 @@
+while True:
+    bad_answer = False
+    ah_item = input("What is the auction for? ")
+    try:
+        reserve = int(input("What is the reserve price? "))
+        if reserve < 0:
+            raise ValueError
+
+    except ValueError:
+        bad_answer = True
+        print("Invalid answer try again")
+    if bad_answer is False:
+        bid = 0
+        current_price = 0
+        while bid != -1:
+            bid = int(input(f"(Current price {current_price})\nYour bid for {ah_item}:"))
+            if bid > current_price:
+                print("Bid success")
+                current_price = bid
+            elif bid > current_price:
+                print("Your bid was lower than the current price")
+        if reserve < current_price:
+            print(f"{ah_item} sold successfully at {current_price:.2f}$")
+        elif reserve < current_price:
+            print(f"{ah_item} didn't meet the {reserve:.2f}$")
