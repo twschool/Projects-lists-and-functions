eb4a7cd4fe78dca8baae5fd047aafd56183d9245
twschool <98859425+twschool@users.noreply.github.com>
2023-02-14 15:02:18 +1300

    Main
    This took me all period to make

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+child_list = []
+per_hour = 12.5
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def total_children():
+    global child_list
+    child_count = 0
+    for child_ in child_list:
+        child_count += 1
+    return child_count
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def print_roll():
+    print(f"Total kids: {total_children()}")
+    global child_list
+    for child_ in child_list:
+        print(child_)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+while True:
+    print("Welcome")
+    error_ = False
+    try:
+        option = int(input("Check in: (1)\nCheck out: (2)\nCalculate cost: (3)"
+                           "\nPrint roll: (4)\nExit: (5)\nOption: "))
+    except ValueError:
+        print("Invalid answer")
+        error_ = False
+    if option == 1:
+        child_name = input("Childs name to check in: ")
+        child_list.append(child_name)
+    elif option == 2:
+        child_name = input("Childs name to check out: ")
+        if child_name in child_list:
+            child_list.remove(child_name)
+            print("Child removed successfully")
+        else:
+            print("Child wasn't found in the daycare")
+    elif option == 3:
+        print(f"Cost for all kids: {total_children() * per_hour}")
+    elif option == 4:
+        print_roll()
+    elif option == 5:
+        exit("Program exited")
+    else:
+        print("Invalid answer")
